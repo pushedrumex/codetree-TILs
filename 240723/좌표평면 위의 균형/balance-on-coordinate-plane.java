b12b96979d0e965692a6f814a8b819b7a494bbad
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-07-23 16:04:53 +0900

    Title: 좌표평면 위의 균형, Time: 233ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240723/좌표평면 위의 균형/balance-on-coordinate-plane.java
+++ 240723/좌표평면 위의 균형/balance-on-coordinate-plane.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int M;
     static Point[] points;
 
     public static void main(String[] args) throws Exception {
@@ -13,69 +12,43 @@ public class Main {
         int R = Integer.parseInt(st.nextToken());
 
         points = new Point[N];
-        M = N;
         for (int i=0;i<N;i++) {
             st = new StringTokenizer(br.readLine());
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             points[i] = new Point(x, y);
         }
-        dfs(0, R, 0, R);
+        int M = N;
+        for (Point point: points) {
+            M = Math.min(M, count(point.x+1, point.y+1));
+            M = Math.min(M, count(point.x+1, point.y-1));
+            M = Math.min(M, count(point.x-1, point.y+1));
+            M = Math.min(M, count(point.x-1, point.y-1));
+        }
         System.out.println(M);
     }
 
-    // dfs + 이분탐색
-    static void dfs(int left, int right, int down, int up) {
-
-        if (left > right || down > up) {
-            return;
-        }
-
-        int xMid = (left + right) / 2;
-        int yMid = (down + up) / 2;
-        if (xMid % 2 != 0) {
-            xMid++;
-        }
-        if (yMid % 2 != 0) {
-            yMid++;
-        }
-
-        int ulCount = 0;
-        int urCount = 0;
-        int dlCount = 0;
-        int drCount = 0;
+    static int count(int x, int y) {
+        int c1 = 0;
+        int c2 = 0;
+        int c3 = 0;
+        int c4 = 0;
         for (Point point: points) {
-            if (point.y > yMid) {
-                if (point.x < xMid) {
-                    ulCount++;
+            if (point.y > y) {
+                if (point.x < x) {
+                    c1++;
                 } else {
-                    urCount++;
+                    c2++;
                 }
             } else {
-                if (point.x < xMid) {
-                    dlCount++;
+                if (point.x < x) {
+                    c3++;
                 } else {
-                    drCount++;
+                    c4++;
                 }
             }
         }
-
-        int maxCount = Arrays.stream(new int[]{ulCount, urCount, dlCount, drCount}).max().getAsInt();
-        M = Math.min(M, maxCount);
-
-        if (maxCount == ulCount) {
-            dfs(left, xMid-2, down, up);
-            dfs(left, right, yMid+2, up);
-        } else if (maxCount == urCount) {
-            dfs(xMid+2, right, down, up);
-            dfs(left, right, yMid+2, up);
-        } else if (maxCount == dlCount) {
-            dfs(left, right, down, yMid-2);
-            dfs(left, xMid-2, down, up);
-        } else {
-            dfs(xMid+2, right, down, up);
-            dfs(left, right, down, yMid-2);
-        }
+        return Arrays.stream(new int[]{c1, c2, c3, c4}).max().getAsInt();
     }
     
     static class Point {
