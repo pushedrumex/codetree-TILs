1a0f677fa2cbb9473546a9813bee444a3b8e301f
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-07-24 13:49:59 +0900

    Title: 2개씩 합치기, Time: 288ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 240724/2개씩 합치기/combine-two.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i=0;i<n;i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num: nums) {
+            pq.add(num);
+        }
+
+        int cost = 0;
+        while (pq.size() > 1) {
+            int num1 = pq.remove();
+            int num2 = pq.remove();
+            int sum = num1 + num2;
+            cost += sum;
+            pq.add(sum);
+        }
+        System.out.println(cost);
+        // 여기에 코드를 작성해주세요.
+    }
+}
