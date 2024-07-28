ee03c9d12414083a6da81ded1fbe952774a95d0a
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-07-28 16:39:11 +0900

    Title: 편안한 핀셋, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240728/편안한 핀셋/comfortable-tweezers.java
+++ 240728/편안한 핀셋/comfortable-tweezers.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    
+
     static int[][] dxdy = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
     public static void main(String[] args) throws Exception {
@@ -10,8 +10,8 @@ public class Main {
         StringTokenizer st;
 
         int N = Integer.parseInt(br.readLine());
-        boolean[][] graph = new boolean[N][N];
-        int[][] pinCount = new int[N][N];
+        boolean[][] graph = new boolean[1000][1000];
+        int[][] pinCount = new int[1000][1000];
 
         int answer = 0;
         for (int k=0;k<N;k++) {
