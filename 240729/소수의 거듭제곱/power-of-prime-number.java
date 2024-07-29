d944c4e6030891850685b08f76ec9c4b5fe069ad
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-07-29 15:56:16 +0900

    Title: 소수의 거듭제곱, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240729/소수의 거듭제곱/power-of-prime-number.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
+        int max = (int)Math.pow(b, 0.5);
+
+        boolean[] isPrime = new boolean[max+1];
+        Arrays.fill(isPrime, true);
+        isPrime[1] = false;
+        for (int i=2;i<=max;i++) {
+            if (isPrime[i] == true) {
+                for (int j=i*2;j<=max;j+=i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        long answer = 0;
+        for (int i=2;i<=max;i++) {
+            if (!isPrime[i]) {
+                continue;
+            }
+
+            int j = i;
+            while (j < b) {
+                j *= i;
+                if (j >= a && j <= b) {
+                    answer++;
+                }
+            }
+        }
+        System.out.println(answer);
+        
+        // 여기에 코드를 작성해주세요.
+    }
+}
