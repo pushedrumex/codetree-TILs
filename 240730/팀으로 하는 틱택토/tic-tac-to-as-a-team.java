6580f3a05f89596af303c1a549ebe77997bdfb71
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-07-30 13:51:30 +0900

    Title: 팀으로 하는 틱택토, Time: 97ms, Memory: 9MB, Status: Passed - CodeTree

--- 240730/팀으로 하는 틱택토/tic-tac-to-as-a-team.java
+++ 240730/팀으로 하는 틱택토/tic-tac-to-as-a-team.java
@@ -4,46 +4,54 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        char[][] board = new char[3][];
+        String[][] board = new String[3][3];
         for (int i=0;i<3;i++) {
-            board[i] = br.readLine().toCharArray();
+            board[i] = br.readLine().split("");
         }
         int one = 0;
         int two = 0;
+
+        HashSet<String> visited = new HashSet<>();
         // 가로
-        for (char[] row: board) {
-            HashSet<Character> set = new HashSet<>();
-            for (char c: row) {
+        for (String[] row: board) {
+            HashSet<String> set = new HashSet<>();
+            for (String c: row) {
                 set.add(c);
             }
-            if (set.size() == 1) {
+            if (set.size() == 1 && !visited.contains(String.join("", set))) {
+                visited.add(String.join("", set));
                 one++;
-            } else if (set.size() == 2) {
+            } else if (set.size() == 2 && !visited.contains(String.join("", set))) {
+                visited.add(String.join("", set));
                 two++;
             }
         }
         // 새로
         for (int j=0;j<3;j++) {
-            HashSet<Character> set = new HashSet<>();
+            HashSet<String> set = new HashSet<>();
             for (int i=0;i<3;i++) {
                 set.add(board[i][j]);
             }
-            if (set.size() == 1) {
+            if (set.size() == 1 && !visited.contains(String.join("", set))) {
+                visited.add(String.join("", set));
                 one++;
-            } else if (set.size() == 2) {
+            } else if (set.size() == 2 && !visited.contains(String.join("", set))) {
+                visited.add(String.join("", set));
                 two++;
             }
         }
         // 대각선
         int i=0;
         int j=0;
-        HashSet<Character> set = new HashSet<>();
+        HashSet<String> set = new HashSet<>();
         for (int k=0;k<3;k++) {
             set.add(board[i+k][j+k]);
         }
-        if (set.size() == 1) {
+        if (set.size() == 1 && !visited.contains(String.join("", set))) {
+            visited.add(String.join("", set));
             one++;
-        } else if (set.size() == 2) {
+        } else if (set.size() == 2 && !visited.contains(String.join("", set))) {
+            visited.add(String.join("", set));
             two++;
         }
 
@@ -53,14 +61,15 @@ public class Main {
         for (int k=0;k<3;k++) {
             set.add(board[i+k][j-k]);
         }
-        if (set.size() == 1) {
+        if (set.size() == 1 && !visited.contains(String.join("", set))) {
+            visited.add(String.join("", set));
             one++;
-        } else if (set.size() == 2) {
+        } else if (set.size() == 2 && !visited.contains(String.join("", set))) {
+            visited.add(String.join("", set));
             two++;
         }
 
         System.out.println(one);
         System.out.println(two);
-        // 여기에 코드를 작성해주세요.
     }
 }
