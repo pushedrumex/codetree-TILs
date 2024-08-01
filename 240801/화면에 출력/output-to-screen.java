f00a97d34bfd7148aa1e0daa9ad9868b6d9308ed
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-08-01 17:23:08 +0900

    Title: 화면에 출력, Time: 1674ms, Memory: 176MB, Status: Memory Limit Exceed - CodeTree

--- 240801/화면에 출력/output-to-screen.java
+++ 240801/화면에 출력/output-to-screen.java
@@ -5,24 +5,37 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int s = Integer.parseInt(br.readLine());
-        int a = 1; // 화면 문자 개수
-        int b = 0; // 클립 보드 문자 개수
-        int answer = 0;
-        for (int i=2;i<=s;i++) {
-            while (true) {
-                answer++;
-                if (b > 0 && a + b == i) {
-                    a += b;
-                    break;
-                } else if (a > 1 && a-1 == i) {
-                    a--;
-                    break;
-                } else {
-                    b = a;
-                }
-            }
-        }
-        System.out.println(answer);
 
+        ArrayDeque<Node> dq = new ArrayDeque<>();
+        dq.add(new Node(1, 0, 0));
+
+        while (!dq.isEmpty()) {
+            Node node = dq.removeFirst();
+            int a = node.a;
+            int b = node.b;
+            int count = node.count;
+            if (a == s) {
+                System.out.println(count);
+                break;
+            }
+            if (b > 0) {
+                dq.add(new Node(a+b, b, count+1));
+            }
+            if (a > 1) {
+                dq.add(new Node(a-1, b, count+1));
+            }
+            dq.add(new Node(a, a, count+1));
+        }
+
+    }
+    static class Node {
+        int a;
+        int b;
+        int count;
+        Node(int a, int b, int count) {
+            this.a = a;
+            this.b = b;
+            this.count = count;
+        }
     }
 }
