81dcbefaffdbd0ca4e2fbf25bd553c44da8e10d3
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-08-09 13:42:15 +0900

    Title: 식의 표기법 바꾸기, Time: 104ms, Memory: 8MB, Status: Passed - CodeTree

--- 240809/식의 표기법 바꾸기/replace-notation-of-expressions.java
+++ 240809/식의 표기법 바꾸기/replace-notation-of-expressions.java
@@ -4,17 +4,42 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] arr = br.readLine().toCharArray();
-        LinkedList<String> operand = new LinkedList();
-        LinkedList<String> operator = new LinkedList();
+        HashMap<Character, Integer> priority = new HashMap<>();
+        priority.put('+', 1);
+        priority.put('-', 1);
+        priority.put('*', 2);
+        priority.put('/', 2);
+        priority.put('(', 0);
 
+        Stack<Character> stack = new Stack<>();
+        String answer = "";
         for (char c: arr) {
-            if (c == '(' || c == ')') continue;
-            if (c >= 'A' && c <= 'Z') {
-                operand.addLast(String.valueOf(c));
-            } else {
-                operator.addFirst(String.valueOf(c));
+            switch (c){
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                    while (!stack.isEmpty() && priority.get(stack.peek()) >= priority.get(c)) {
+                        answer += String.valueOf(stack.pop());
+                    }
+                    stack.add(c);
+                    break;
+                case '(':
+                    stack.add(c);
+                    break;
+                case ')':
+                    while(!stack.isEmpty() && stack.peek() != '('){
+                        answer += String.valueOf(stack.pop());
+                    }
+                    stack.pop();
+                    break;
+                default:
+                    answer += String.valueOf(c);
             }
         }
-        System.out.println(String.join("", operand) + String.join("", operator));
+        while (!stack.isEmpty()) {
+            answer += String.valueOf(stack.pop());
+        }
+        System.out.println(answer);
     }
 }
