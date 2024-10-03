5533a5306265917ba9f5d1f299189d97e721b12f
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-10-03 20:36:03 +0900

    Title: 정수 두 개의 크기 비교, Time: 269ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241003/정수 두 개의 크기 비교/compare-the-size-of-two-integers.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n;
+    static int[] nums;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        n = Integer.parseInt(br.readLine());
+        nums = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i=0;i<n;i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+        int answer = 0;
+        for (int i=1;i<n;i++) {
+            answer += i-bs(i);
+        }
+        System.out.println(answer);
+    }
+
+    static int bs(int i) {
+        int answer = i;
+        int left = 0;
+        int right = i-1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] >= (float)nums[i] * 0.9) {
+                answer = mid;
+                right = mid-1;
+            } else {
+                left = mid+1;
+            }
+        }
+        return answer;
+    }
+}
