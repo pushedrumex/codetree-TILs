a2a91ec1f1896a5f9f37a81c9fbfc69d9495b294
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-10-08 18:14:18 +0900

    Title: Carry 피하기, Time: 3054ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

---
+++ 241008/Carry 피하기/escaping-carry.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] nums;
+    static int answer = 0;
+    static int n;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        nums = new int[n];
+        for (int i=0;i<n;i++) {
+            nums[i] = Integer.parseInt(br.readLine());
+        }
+
+        dfs(0, 0, 0);
+
+        System.out.println(answer);
+    }
+
+    static void dfs(int i, int sum, int count) {
+        if (i == n) {
+            answer = Math.max(answer, count);
+            return;
+        }
+
+        for (int j=i;j<n;j++) {
+            // 선택 가능한 숫자라면 선택
+            if (isNotCarry(sum, nums[i])) {
+                dfs(i+1, sum + nums[i], count+1);
+            }
+            // 현재 숫자 선택 X
+            dfs(i+1, sum, count);
+        }
+
+    }
+
+    static boolean isNotCarry(int n1, int n2) {
+        if (n1 == 0 || n2 == 0) return true;
+        if (n1 % 10 + n2 % 10 > 9) return false;
+        return isNotCarry(n1 / 10, n2 / 10);
+    }
+}
