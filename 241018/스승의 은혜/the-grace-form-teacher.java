9f2b7b6c6b80c87f8e199513f23529f93483c5ca
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-10-18 16:42:14 +0900

    Title: 스승의 은혜, Time: 233ms, Memory: 13MB, Status: Wrong Answer - CodeTree

---
+++ 241018/스승의 은혜/the-grace-form-teacher.java
@@ -0,0 +1,61 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static Present[] presents;
+    static int N;
+    static int B;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        B = Integer.parseInt(st.nextToken());
+
+        presents = new Present[N];
+        for (int i=0;i<N;i++) {
+            st = new StringTokenizer(br.readLine());
+            int cost = Integer.parseInt(st.nextToken());
+            int delivery = Integer.parseInt(st.nextToken());
+            presents[i] = new Present(cost, delivery);
+        }
+        
+        int answer = 0;
+        // 하나씩 할인해보자
+        for (int i=0;i<N;i++) {
+            presents[i].cost /= 2;
+
+            Arrays.sort(presents, (o1, o2) -> ((int)o1.cost + o1.delivery) - ((int)o2.cost + o2.delivery));
+            answer = Math.max(answer, countMax());
+
+            presents[i].cost *= 2;
+        }
+
+        System.out.println(answer);
+    }
+
+    static int countMax() {
+        int count = 0;
+        int money = B;
+        for (int i=0;i<N;i++) {
+            double totalCost = presents[i].cost + presents[i].delivery;
+            if (money >= totalCost) {
+                money -= totalCost;
+                count++;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+
+    static class Present {
+        double cost;
+        int delivery;
+        Present(double cost, int delivery) {
+            this.cost = cost;
+            this.delivery = delivery;
+        }
+    }
+}
