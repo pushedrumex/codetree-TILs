e7074098e099ddc6f8585fa3e0e493ef4456f413
minjungkim <97938489+pushedrumex@users.noreply.github.com>
2024-10-31 13:51:27 +0900

    Title: 동전 가치의 합, Time: 92ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 241031/동전 가치의 합/sum-of-coin-values.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i=0;i<n;i++) {
+            coins[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[m+1];
+        dp[0] = 1;
+        // i번째 동전으로
+        for (int i=0;i<n;i++) {
+            int coin = coins[i];
+            // j원 만들기
+            for (int j=coin;j<=m;j++) {
+                dp[j] += dp[j-coin];
+            }
+        }
+
+        System.out.println(dp[m]);
+    }
+}
